6e73e6c7fbcc69b1fc11b3debdc2d1d1c3bb7f81
Youcef <youcefcopy@gmail.com>
2025-10-25 21:06:48 +0100

    ClassList toggle is way more convanient

--- SideBar/script.js
+++ SideBar/script.js
@@ -1,8 +1,27 @@
-const hide = document.getElementById(`hide`);
+// const hide = document.getElementById(`hide`);
 
-const aside = document.getElementById(`aside`);
+// const aside = document.getElementById(`aside`);
 
-hide.onclick = function (event) {
-     event.preventDefault(); // prevent jumping
-     aside.classList.add(`hidden`);
+// let sideBarHidden = false;
+
+
+// hide.onclick = function (event) {
+//     event.preventDefault(); // prevent jump if button is inside <a> or something
+//     if (sideBarHidden) {
+//         aside.classList.add(`aside`);
+//     } else {
+//         aside.classList.add(`hidden`);
+
+//     }
+//     sideBarHidden = !sideBarHidden;  // flip the state every time the button is clicked
+
+// }
+
+
+const hide = document.getElementById("hide");
+const aside = document.getElementById("aside");
+
+hide.onclick = (event) => {
+  event.preventDefault();
+  aside.classList.toggle("hidden");
 }
